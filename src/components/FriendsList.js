4d3fccdaa310e2c33f4dfe51cf69dd4b9ce4aeaf
Joseph Blank <joe.blank@live.com>
2016-11-14 17:21:27 -0700

    added header and code for inputs and menus on FriendsList

---
+++ src/components/FriendsList.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+class FriendsList extends React.Component {
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+              searchText: ""
+            , orderBy: "name"
+            , order: "ascending"
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <form
+                    className="form-inline searchForm"
+                    role="form"
+                >
+                    <div className="form-group">
+
+                        <input
+                            className="form-control"
+                            placeholder="Search Anything For Friends"
+                            value={ this.state.searchText }
+                        />
+
+                        <select
+                            className="input-medium"
+                            value={ this.state.orderBy }
+                        >
+                            <option value="name">Name</option>
+                            <option value="friend_count">#Friends</option>
+                        </select>
+
+                        <select
+                            className="input-medium"
+                            value={ this.state.order }
+                        >
+                            <option value="descending">Descending</option>
+                            <option value="ascending">Ascending</option>
+                        </select>
+
+                    </div>
+                </form>
+
+                <ul>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default FriendsList;
